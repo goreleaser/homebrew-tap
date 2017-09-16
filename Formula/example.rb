a1329f9ef18e4a4567fc0bb94a048e6a8c15aa5c
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-16 20:03:21 -0300

    example version v0.0.4

--- Formula/example.rb
+++ Formula/example.rb
@@ -1,9 +1,9 @@
 class Example < Formula
   desc "GoReleaser example repository"
   homepage "http://goreleaser.github.io"
-  url "https://github.com/goreleaser/example/releases/download/v0.0.3/example_0.0.3_darwin_amd64.tar.gz"
-  version "0.0.3"
-  sha256 "1587e1b0a07432c7fd38347e7b36434de3b964db3ea549caa1db971d13facb0c"
+  url "https://github.com/goreleaser/example/releases/download/v0.0.4/example_0.0.4_darwin_amd64.tar.gz"
+  version "0.0.4"
+  sha256 "ddbdce771bf623b30bf2e342e86b5344fb209fb4d432017a724c1b41a903fc71"
 
   def install
     bin.install "example"
