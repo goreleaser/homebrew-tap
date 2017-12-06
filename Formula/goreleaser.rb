504860cf551da797b01ba07ac02bf3638fdeb7ae
goreleaserbot <goreleaser@carlosbecker.com>
2017-12-06 00:24:57 -0200

    goreleaser version v0.37.6

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -1,9 +1,9 @@
 class Goreleaser < Formula
   desc "Deliver Go binaries as fast and easily as possible"
   homepage "http://goreleaser.github.io"
-  url "https://github.com/goreleaser/goreleaser/releases/download/v0.37.5/goreleaser_Darwin_x86_64.tar.gz"
-  version "0.37.5"
-  sha256 "4676bfece582bae9384a8c5f24735fb6ff5f725cedcd867412dabfa1bc08982f"
+  url "https://github.com/goreleaser/goreleaser/releases/download/v0.37.6/goreleaser_Darwin_x86_64.tar.gz"
+  version "0.37.6"
+  sha256 "12e17abb21200f4175a34bd227c31ddb83f791d96093caf4ce5d533f00c894b5"
 
   def install
     bin.install "goreleaser"
