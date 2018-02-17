22eea5ed40ccbc8e300546ad83152314afd89c12
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-16 22:59:54 -0200

    nfpm version v0.3.0

--- Formula/nfpm.rb
+++ Formula/nfpm.rb
@@ -3,7 +3,7 @@ class Nfpm < Formula
   homepage "https://github.com/goreleaser/nfpm"
   url "https://github.com/goreleaser/nfpm/releases/download/v0.3.0/nfpm_0.3.0_Darwin_x86_64.tar.gz"
   version "0.3.0"
-  sha256 "36282cb590e03b9669a382903a44dd76cbaa2c564b753bb6d464b30f6cb0ef7b"
+  sha256 "15f6ff566221fdcec78c49f16849a4f7ec2da46c0fe6b20e022e08dd00fdee94"
 
   def install
     bin.install "nfpm"
