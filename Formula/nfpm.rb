ea55644a3b3fbda35fafae4e9be046e29be7ca5a
goreleaserbot <goreleaser@carlosbecker.com>
2018-02-28 08:32:01 -0300

    nfpm version v0.5.5

--- Formula/nfpm.rb
+++ Formula/nfpm.rb
@@ -1,9 +1,9 @@
 class Nfpm < Formula
   desc "NFPM is not FPM"
   homepage "https://github.com/goreleaser/nfpm"
-  url "https://github.com/goreleaser/nfpm/releases/download/v0.5.4/nfpm_0.5.4_Darwin_x86_64.tar.gz"
-  version "0.5.4"
-  sha256 "6ca719dbc54e2648c420442d0c8fe3f0b3fd5b27713efff0b9f4c312e7d92f04"
+  url "https://github.com/goreleaser/nfpm/releases/download/v0.5.5/nfpm_0.5.5_Darwin_x86_64.tar.gz"
+  version "0.5.5"
+  sha256 "460b57145e810350d7ac52719e8da6ea6569c17dfee3671426759950b5ac1247"
 
   def install
     bin.install "nfpm"
