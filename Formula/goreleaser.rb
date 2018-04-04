5b8f6720ebbc9b858ab5df045a83efc8d1b5868f
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-04 08:16:33 -0300

    Brew formula update for goreleaser version v0.65.0

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -1,9 +1,9 @@
 class Goreleaser < Formula
   desc "Deliver Go binaries as fast and easily as possible"
   homepage "https://goreleaser.com"
-  url "https://github.com/goreleaser/goreleaser/releases/download/v0.64.0/goreleaser_Darwin_x86_64.tar.gz"
-  version "0.64.0"
-  sha256 "62dbe1f10a10aeb96f7eaa33a883d0c2462e7d2c437488e00d64b760ade21fc6"
+  url "https://github.com/goreleaser/goreleaser/releases/download/v0.65.0/goreleaser_Darwin_x86_64.tar.gz"
+  version "0.65.0"
+  sha256 "1073ca6a942a55b74562deef811ed6125a952920d8dae13a4a37bfe9bcbb6fe7"
 
   def install
     bin.install "goreleaser"
