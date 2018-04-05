a3955fdf828c66e645c85e125745eda9035f3bbf
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-05 09:56:48 -0300

    Brew formula update for nfpm version v0.6.3

--- Formula/nfpm.rb
+++ Formula/nfpm.rb
@@ -1,12 +1,17 @@
 class Nfpm < Formula
   desc "NFPM is not FPM"
   homepage "https://github.com/goreleaser/nfpm"
-  url "https://github.com/goreleaser/nfpm/releases/download/v0.6.2/nfpm_0.6.2_Darwin_x86_64.tar.gz"
-  version "0.6.2"
-  sha256 "8ce85c892f6d7076479db4e297dd17813bae3519374422526dc4e11a9646a86d"
+  url "https://github.com/goreleaser/nfpm/releases/download/v0.6.3/nfpm_0.6.3_Darwin_x86_64.tar.gz"
+  version "0.6.3"
+  sha256 "89049edb27856e761acf93491a5fd27f72cf5498f2a536a67bc14102c727ec42"
 
   def install
     bin.install "nfpm"
+  end
+
+  def caveats; <<-EOS.undent
+    
+  EOS
   end
 
   test do
