2cbe565454c4ebfff9a743bb6be11e2e548301fd
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-06 12:28:00 -0300

    Brew formula update for goreleaser version v0.143.0

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -2,24 +2,24 @@
 class Goreleaser < Formula
   desc "Deliver Go binaries as fast and easily as possible"
   homepage "https://goreleaser.com"
-  version "0.142.0"
+  version "0.143.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/goreleaser/goreleaser/releases/download/v0.142.0/goreleaser_Darwin_x86_64.tar.gz"
-    sha256 "d3153bf3bdf1b943682a2def02852a3377ec63813be0c1c034e1e92d58494393"
+    url "https://github.com/goreleaser/goreleaser/releases/download/v0.143.0/goreleaser_Darwin_x86_64.tar.gz"
+    sha256 "0b713827a2c0e21238a211899e6eb81f23c2dc7c9b39ecb426b06ed6efbce568"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/goreleaser/goreleaser/releases/download/v0.142.0/goreleaser_Linux_x86_64.tar.gz"
-      sha256 "eb61a73f5b0947abb8d85074b7bcfd06460f869f3c5708c73800bd57668e55a2"
+      url "https://github.com/goreleaser/goreleaser/releases/download/v0.143.0/goreleaser_Linux_x86_64.tar.gz"
+      sha256 "cc435eb337889d41414de80fd8474806187a3e908754cbf4599aa0a7604a3134"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/goreleaser/goreleaser/releases/download/v0.142.0/goreleaser_Linux_arm64.tar.gz"
-        sha256 "5b18b803b07423b3873c9e04de8b8de62b7d5dec6938b368b771d66a6aee833c"
+        url "https://github.com/goreleaser/goreleaser/releases/download/v0.143.0/goreleaser_Linux_arm64.tar.gz"
+        sha256 "cc9f0f9676a5022391e36077c004605f0a64d09d83667a8cb75f0b3fc4e6d5b5"
       else
-        url "https://github.com/goreleaser/goreleaser/releases/download/v0.142.0/goreleaser_Linux_armv6.tar.gz"
-        sha256 "6997ac9a8da0bf21509da5bfdbb8982ef725d4829eb0f7286843de37d3f2b271"
+        url "https://github.com/goreleaser/goreleaser/releases/download/v0.143.0/goreleaser_Linux_armv6.tar.gz"
+        sha256 "4a08f54d5c53090db67f91290cbccb65db4b91307522f3ed112e7d82c9badc5e"
       end
     end
   end
