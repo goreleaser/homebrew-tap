951e0e2ff66d3a84770168ebe70e3827ca17c7df
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-26 13:40:12 -0300

    Brew formula update for goreleaser-pro version v0.166.0-pro

--- Formula/goreleaser-pro.rb
+++ Formula/goreleaser-pro.rb
@@ -11,23 +11,23 @@ class GoreleaserPro < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/goreleaser/goreleaser-pro/releases/download/v0.166.0-pro/goreleaser-pro_Darwin_x86_64.tar.gz"
-    sha256 "8cb2bd32de1bfcb04469af6d3bd87bff2135910351af3ec646b8dbba08ded705"
+    sha256 "7006b7f400f6041691f6d97b72dc5beb392abb7d7874c0fbc1e9d51ce2b59e23"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/goreleaser/goreleaser-pro/releases/download/v0.166.0-pro/goreleaser-pro_Darwin_arm64.tar.gz"
-    sha256 "ff0fa0b88ec827a4776511b20ea75e033e6fc514be36188fa3101d54f2f2969f"
+    sha256 "3a406405e2414c83cda38bd0e9cee535598e6e6863b8f77d3360d5df34f34229"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/goreleaser/goreleaser-pro/releases/download/v0.166.0-pro/goreleaser-pro_Linux_x86_64.tar.gz"
-    sha256 "f94cf1b026b00adf4dd1bbe6d098d4f2a0049d7bc827ed2cc495d3a0767f6cd5"
+    sha256 "92a20ea471a88e4b1b56db61a91756a5ee556157485bdbf6df1cf26b44721658"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
     url "https://github.com/goreleaser/goreleaser-pro/releases/download/v0.166.0-pro/goreleaser-pro_Linux_armv6.tar.gz"
-    sha256 "aafc81ccb5c903eac3f5358c4ff285fd6c2a2aee68cf2b659cb36e9221d5c08b"
+    sha256 "27d4dad647b326a515bf536add5bce57b523b99ea7bb71ffd861daebb1d45811"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/goreleaser/goreleaser-pro/releases/download/v0.166.0-pro/goreleaser-pro_Linux_arm64.tar.gz"
-    sha256 "c27b322f2b7c4ca60483609f7433fe608e68543f7a2059d60b2c5b61cbe204b0"
+    sha256 "c4d2a726143e14b6686a3ae3e790e55693eba0b997cb46fefb8c1267e3718477"
   end
 
   depends_on "go" => :optional
