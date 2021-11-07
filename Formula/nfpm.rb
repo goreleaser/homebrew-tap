e07b01356b52651debd10080c327c640f2d31a4c
goreleaserbot <goreleaser@carlosbecker.com>
2021-11-07 00:49:11 -0300

    Brew formula update for nfpm version v2.8.0

--- Formula/nfpm.rb
+++ Formula/nfpm.rb
@@ -5,37 +5,57 @@
 class Nfpm < Formula
   desc "nFPM is a simple, 0-dependencies, deb, rpm and apk packager."
   homepage "https://nfpm.goreleaser.com"
-  version "2.7.1"
+  version "2.8.0"
   license "MIT"
-  bottle :unneeded
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/goreleaser/nfpm/releases/download/v2.7.1/nfpm_2.7.1_Darwin_arm64.tar.gz"
-      sha256 "aab245e414edfc8efe976292cb08a52b0ec1fd2f81214615d132c2ed68d34f95"
+      url "https://github.com/goreleaser/nfpm/releases/download/v2.8.0/nfpm_2.8.0_Darwin_arm64.tar.gz"
+      sha256 "26c4aee3d49df4a5d3c592ba4fc02a2ea6e71e1d5302d21ebd12556ed5c90575"
+
+      def install
+        bin.install "nfpm"
+        bash_completion.install "completions/nfpm.bash" => "nfpm"
+        zsh_completion.install "completions/nfpm.zsh" => "_nfpm"
+        fish_completion.install "completions/nfpm.fish"
+      end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/goreleaser/nfpm/releases/download/v2.7.1/nfpm_2.7.1_Darwin_x86_64.tar.gz"
-      sha256 "6d8ec6285703ffdd755440540782acf696d91f3a84cda242d0c13910337ebac5"
+      url "https://github.com/goreleaser/nfpm/releases/download/v2.8.0/nfpm_2.8.0_Darwin_x86_64.tar.gz"
+      sha256 "ba8283ec45960d93c319daf227176b669a11741a354659bececf906ff1171ca2"
+
+      def install
+        bin.install "nfpm"
+        bash_completion.install "completions/nfpm.bash" => "nfpm"
+        zsh_completion.install "completions/nfpm.zsh" => "_nfpm"
+        fish_completion.install "completions/nfpm.fish"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/goreleaser/nfpm/releases/download/v2.7.1/nfpm_2.7.1_Linux_arm64.tar.gz"
-      sha256 "258426c7be38d363de330353bdeff30fb4d55b0dc5ba27da663413a2b6326d6e"
+      url "https://github.com/goreleaser/nfpm/releases/download/v2.8.0/nfpm_2.8.0_Linux_arm64.tar.gz"
+      sha256 "0db45be7c34d6ea1955710dd71eefc5a79a488e7ee44d816407d366a511ced6a"
+
+      def install
+        bin.install "nfpm"
+        bash_completion.install "completions/nfpm.bash" => "nfpm"
+        zsh_completion.install "completions/nfpm.zsh" => "_nfpm"
+        fish_completion.install "completions/nfpm.fish"
+      end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/goreleaser/nfpm/releases/download/v2.7.1/nfpm_2.7.1_Linux_x86_64.tar.gz"
-      sha256 "32e2152c6c84f292206934153d536e5999213faf35795510c5085db11b04372c"
-    end
-  end
+      url "https://github.com/goreleaser/nfpm/releases/download/v2.8.0/nfpm_2.8.0_Linux_x86_64.tar.gz"
+      sha256 "13ef1215b0aced88b9f96cb6e35f9b0647eb70ecbf22c0af87b7c2de2c6e9b07"
 
-  def install
-    bin.install "nfpm"
-    bash_completion.install "completions/nfpm.bash" => "nfpm"
-    zsh_completion.install "completions/nfpm.zsh" => "_nfpm"
-    fish_completion.install "completions/nfpm.fish"
+      def install
+        bin.install "nfpm"
+        bash_completion.install "completions/nfpm.bash" => "nfpm"
+        zsh_completion.install "completions/nfpm.zsh" => "_nfpm"
+        fish_completion.install "completions/nfpm.fish"
+      end
+    end
   end
 
   test do
